a6abdd83247066d5dc3c8d4a1114aeb2b40cae1d
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-09-29 16:45:09 +0200

    setup guard

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -4,12 +4,20 @@ import { MemberListComponent } from './components/members/member-list/member-lis
 import { MemberDetailsComponent } from './components/members/member-details/member-details.component';
 import { ListsComponent } from './components/lists/lists.component';
 import { MessagesComponent } from './components/messages/messages.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'members', component: MemberListComponent },
-  { path: 'members/:id', component: MemberDetailsComponent },
-  { path: 'lists', component: ListsComponent },
-  { path: 'messages', component: MessagesComponent },
+  {
+    path: '',
+    runGuardsAndResolvers: 'always',
+    canActivate: [authGuard],
+    children: [
+      { path: 'members', component: MemberListComponent },
+      { path: 'members/:id', component: MemberDetailsComponent },
+      { path: 'lists', component: ListsComponent },
+      { path: 'messages', component: MessagesComponent },
+    ],
+  },
   { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
